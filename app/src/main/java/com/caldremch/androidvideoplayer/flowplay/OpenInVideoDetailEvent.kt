b826e95450ab873cfc024ea05ca0bb95099b4009
Caldremch <caldremch@163.com>
2019-05-25 23:48:08 +0800

    add

---
+++ app/src/main/java/com/caldremch/androidvideoplayer/flowplay/OpenInVideoDetailEvent.kt
@@ -0,0 +1,14 @@
+package com.caldremch.androidvideoplayer.flowplay
+
+/**
+ * @author Caldremch
+ *
+ * @date 2019-05-25 23:44
+ *
+ * @email caldremch@163.com
+ *
+ * @describe
+ *
+ **/
+class OpenInVideoDetailEvent {
+}
