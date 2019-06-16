88a87a82d722b69048f1cf7e36f90e85198d7074
Caldremch <caldremch@163.com>
2019-06-16 12:32:39 +0800

    update

--- app/src/main/java/com/caldremch/androidvideoplayer/Activity/CameraActivity.kt
+++ app/src/main/java/com/caldremch/androidvideoplayer/Activity/CameraActivity.kt
@@ -1,5 +1,9 @@
 package com.caldremch.androidvideoplayer.Activity
 
+import android.util.DisplayMetrics
+import android.util.Log
+import android.util.Rational
+import androidx.camera.core.*
 import com.caldremch.androidvideoplayer.R
 import com.caldremch.androidvideoplayer.widget.camera.camera1.CameraManager
 import com.caldremch.common.base.BaseActivity
@@ -7,11 +11,31 @@ import kotlinx.android.synthetic.main.activity_camera.*
 
 class CameraActivity : BaseActivity() {
 
+    private var preView:Preview? =null
+    private var imageCapture:ImageCapture? =null
+    private var imageAnalysis:ImageAnalysis? =null
+
     override fun getLayoutId(): Int {
         return R.layout.activity_camera;
     }
 
     override fun initView() {
-//        CameraManager(surfaceView).open(0)
+        textureView.post {
+            bindCamera()
+        }
+    }
+
+    private fun bindCamera() {
+        CameraX.unbindAll()
+        val metrics = DisplayMetrics().also {
+            textureView.display.getRealMetrics(it)
+        }
+        Log.d(javaClass.simpleName, "Metrics: ${metrics.widthPixels} x ${metrics.heightPixels}")
+        val screenAspectRatio = Rational(metrics.widthPixels, metrics.heightPixels)
+
+        val textureViewConfigy = PreviewConfig.Builder().apply {
+            setLensFacing()
+
+        }.build()
     }
 }
